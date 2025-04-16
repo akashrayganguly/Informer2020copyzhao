9cf27dd231038f65d9f7ca4b1e3eaaadf0784df9
akashrayganguly <52396641+akashrayganguly@users.noreply.github.com>
2025-04-16 16:05:02 +0530

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -65,7 +65,8 @@ class StandardScaler():
     def transform(self, data):
         mean = torch.from_numpy(self.mean).type_as(data).to(data.device) if torch.is_tensor(data) else self.mean
         std = torch.from_numpy(self.std).type_as(data).to(data.device) if torch.is_tensor(data) else self.std
-        return (data - mean) / std
+        return data
+        #return (data - mean) / std
 
     def inverse_transform(self, data):
         mean = torch.from_numpy(self.mean).type_as(data).to(data.device) if torch.is_tensor(data) else self.mean
